989672598fab14a653ed656d122b637d455b9a54
Mannnyyyyuell <165842494+Mannnyyyyuell@users.noreply.github.com>
2025-02-10 19:34:37 -0800

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -5,10 +5,10 @@ brain = vex.Brain()
 def main():
     while True:
         brain.screen.clear_screen()
-        width = (potPinE.value() * 180) // 4095  # Changed scaling factor
+        width = (potPinE.value() * 180) // 4095  
         brain.screen.set_fill_color(vex.Color.GREEN)
-        brain.screen.draw_rectangle(110, 90, width, 40)  # Adjusted position and size
-        brain.screen.print_at(15, 25, "Value: %d" % pot.value())  # Adjusted text position
-        vex.wait(100, vex.TimeUnits.MSEC)  # Changed wait time
+        brain.screen.draw_rectangle(110, 90, width, 40)  
+        brain.screen.print_at(15, 25, "Value: %d" % pot.value())  
+        vex.wait(100, vex.TimeUnits.MSEC)  
 
 main()
